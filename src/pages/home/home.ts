25966004779ffd7c8b4663be268a21c633576f64
Álvaro Bunster <al.bunstr@gmail.com>
2018-10-18 00:22:29 -0300

    Se consulta listado de objetos en la base de datos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 
 import {SubirPage} from "../subir/subir";
+
+// firebase
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts: Observable<any[]>;
 
-  constructor(private modalCtrl: ModalController) {
-
+  constructor(private modalCtrl: ModalController,
+              private afDB: AngularFireDatabase) {
+    this.posts = afDB.list('post').valueChanges();
   }
 
   mostrarModal() {
